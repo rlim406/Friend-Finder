adf654ca4362af1c80af4f45248627b72cc36807
r.lim406 <r.lim406@hotmail.com>
2018-09-04 15:49:23 -0400

    added name, photo and scores to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+  {
+    "name": "Jack Sparrow",
+    "photo": "https://vignette.wikia.nocookie.net/deathbattlefanon/images/8/8a/Jack_Sparrow.jpg/revision/latest?cb=20140905042430",
+    "scores": [5, 1, 3, 2, 5, 5, 1, 1, 2, 3]
+  },
+
+  {
+    "name": "Hermione Granger",
+    "photo": "https://idigitalcitizen.files.wordpress.com/2009/09/hermione-granger-hp6-dvd-320x480-iphone.jpg?w=320",
+    "scores": [2, 4, 3, 5, 1, 2, 4, 3, 5, 2]
+  },
+
+  {
+    "name": "Regina George",
+    "photo": "https://precisioncrossfit.net/wp-content/uploads/2014/08/wpid-d53649791a1a5dc7858fb2a2ca4cc4fb.jpg",
+    "scores": [1, 5, 2, 3, 1, 1, 2, 4, 4, 1]
+  },
+
+  {
+    "name": "Steve Rogers",
+    "photo": "https://wallpapercave.com/wp/wp1880635.jpg",
+    "scores": [2, 1, 4, 5, 5, 3, 1, 1, 2, 2]
+  },
+
+  {
+    "name": "Katniss Everdeen",
+    "photo": "https://dz7u9q3vpd4eo.cloudfront.net/wp-content/legacy/posts/80e53442-04ca-4845-9c5d-0b79c08942da.jpg",
+    "scores": [3, 2, 1, 3, 4, 4, 4, 2, 2, 4]
+  },
+
+  {
+    "name": "Mia Thermopolis",
+    "photo": "http://cdn01.cdn.justjared.com/wp-content/uploads/headlines/2017/08/anne-hathaway-princess-diaries-happy-birthday-social.jpg",
+    "scores": [1, 3, 4, 4, 2, 1, 5, 5, 5, 5]
+  }
+]
